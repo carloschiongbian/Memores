b8854b7015e853dc77f198874ba77884dc9b9437
julcbn <chiongbianpsn@gmail.com>
2022-08-17 08:58:17 +0800

    I added the front-end for the patient records navigation

--- src/patientRecord.js
+++ src/patientRecord.js
@@ -2,6 +2,9 @@ import './public/css/PatientRecord/patientRecord.css';
 import PatientInformation from './components/patientInformation';
 
 const PatientRecord = () => {
+
+    // Data for the patients will be fetched from this page
+
     return (
         <div className="patient-records-container">
             <div className="patient-records-navbar">
@@ -23,6 +26,14 @@ const PatientRecord = () => {
                     <h3>Actions</h3>
                 </div>
                 <PatientInformation />
+            </div>
+
+            <div className="patient-records-navigation">
+                <button>Prev</button>
+                <div className="patient-records-page-display">
+                    1
+                </div>
+                <button>Next</button>
             </div>
         </div>
     );
