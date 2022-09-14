2e407e3ee5971fdaa4275a8582046f7467e66812
JDLawsin <JDlawsin@gmail.com>
2022-09-15 05:11:43 +0800

    setup connection with the database

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,18 +1,36 @@
-#input "python server.py" to run the flask server
-
+# input "python server.py" to run the flask server
 from flask import Flask
+import pymysql
 
-app = Flask(__name__);
+app = Flask(__name__)
 
-#when using routes, use the "@" symbol before the variable
-#which holds the imported module
+connection = pymysql.connect(host='localhost',
+                             user='user',
+                             password='',
+                             database='memores',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+# when using routes, use the "@" symbol before the variable
+# which holds the imported module
 
 # access "localhost:5000/members" to see the server
 # return the members in JSON format
-@app.route("/members")
 
+
+@app.route("/members")
 def members():
-    return { "members": ["Hans","Joshua","Julian","Angelica"] }
+    return {"members": ["Hans", "Joshua", "Julian", "Angelica"]}
+
+
+@app.route('/admin')
+def getAdmin():
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM administrators')
+    data = cursor.fetchall()
+    print(data)
+    return data
+
 
 if __name__ == "__main__":
     app.run(debug=True)
