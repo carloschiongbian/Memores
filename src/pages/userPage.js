1542b4b2e4a20a1ab607688e714f045eccf0d564
Hansel <valkirecesa@gmail.com>
2022-09-21 18:10:35 +0800

    added button for professional license upload

--- src/pages/userPage.js
+++ src/pages/userPage.js
@@ -29,7 +29,7 @@ const UserPage = () => {
             <div className="container">
 
                 {/* Breadcrumbs */}
-                <nav aria-label="breadcrumb" className='py-4 border-bottom d-flex justify-content-between'>
+                <nav aria-label="breadcrumb" className='py-4 border-bottom d-flex justify-content-between flex-grow-1 flex-wrap'>
                     <ol className="breadcrumb mb-0 align-items-center">
                         <li className="breadcrumb-item">
                             <Link to="/userRecord" className='text-decoration-none text-primary fw-bold'>
@@ -66,13 +66,19 @@ const UserPage = () => {
                         <div className="col-12 col-lg-6">
                             <h5 className='fw-bold'>Personal Information</h5>
                             <div>
-                            <label htmlFor="contact-number" className="form-label">Professional license</label>
+                                <label htmlFor="contact-number" className="form-label">Professional license</label>
 
 
                                 <div>
 
                                     <div className='position-relative'>
-                                        <img src={userInfo.licenseImage} alt="" className='professional-license'/>
+                                        <img src={userInfo.licenseImage} alt="" className='professional-license' />
+
+
+                                        <div className='upload-image-inner-container'>
+                                            <input className='inputfile' type="file" name="pic" accept="image/*" style={{ cursor: "pointer" }} />
+                                            <label><i className="bi bi-camera-fill text-primary" height="30" width="30"></i></label>
+                                        </div>
                                     </div>
 
                                 </div>
@@ -82,27 +88,27 @@ const UserPage = () => {
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="license-number" className="form-label">License Number</label>
-                                <input type="text" className="form-control" id="license-number" placeholder="License Number" defaultValue={userInfo.licenseNumber} readOnly/>
+                                <input type="text" className="form-control" id="license-number" placeholder="License Number" defaultValue={userInfo.licenseNumber} readOnly />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="first-name" className="form-label">First Name</label>
-                                <input type="text" className="form-control" id="first-name" placeholder="First Name" defaultValue={userInfo.firstName} readOnly/>
+                                <input type="text" className="form-control" id="first-name" placeholder="First Name" defaultValue={userInfo.firstName} readOnly />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="last-name" className="form-label">Last Name</label>
-                                <input type="text" className="form-control" id="last-name" placeholder="Last Name" defaultValue={userInfo.lastName} readOnly/>
+                                <input type="text" className="form-control" id="last-name" placeholder="Last Name" defaultValue={userInfo.lastName} readOnly />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="email-address" className="form-label">Email Address</label>
-                                <input type="email" className="form-control" id="email-address" placeholder="Email Address" defaultValue={userInfo.emailAddress} readOnly/>
+                                <input type="email" className="form-control" id="email-address" placeholder="Email Address" defaultValue={userInfo.emailAddress} readOnly />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="contact-number" className="form-label">Contact Number</label>
-                                <input type="text" className="form-control" id="contact-number" placeholder="Contact Number" defaultValue={userInfo.contactNumber} readOnly/>
+                                <input type="text" className="form-control" id="contact-number" placeholder="Contact Number" defaultValue={userInfo.contactNumber} readOnly />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="birthdate" className="form-label">Date of Birth</label>
-                                <input type="date" className="form-control" id="birthdate" defaultValue={userInfo.dateOfBirth} readOnly/>
+                                <input type="date" className="form-control" id="birthdate" defaultValue={userInfo.dateOfBirth} readOnly />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="gender" className="form-label">Gender</label>
@@ -120,27 +126,27 @@ const UserPage = () => {
                             <h5 className='fw-bold'>Account Information</h5>
                             <div className="mb-3">
                                 <label htmlFor="username" className="form-label">Username</label>
-                                <input type="text" className="form-control" id="username" placeholder="Username" defaultValue={userInfo.userName} readOnly/>
+                                <input type="text" className="form-control" id="username" placeholder="Username" defaultValue={userInfo.userName} readOnly />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="password" className="form-label">Password</label>
-                                <input type="password" className="form-control" id="password" placeholder="Password" defaultValue={userInfo.password} readOnly/>
+                                <input type="password" className="form-control" id="password" placeholder="Password" defaultValue={userInfo.password} readOnly />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="confirm-password" className="form-label">Confirm Password</label>
-                                <input type="password" className="form-control" id="confirm-password" placeholder="Confirm Password" defaultValue={userInfo.confirmPassword} readOnly/>
+                                <input type="password" className="form-control" id="confirm-password" placeholder="Confirm Password" defaultValue={userInfo.confirmPassword} readOnly />
                             </div>
 
-                            <hr className='my-4'/>
+                            <hr className='my-4' />
 
                             <h5 className='fw-bold'>Address Information</h5>
                             <div className="mb-3">
                                 <label htmlFor="street-address" className="form-label">Street Address</label>
-                                <input type="text" className="form-control" id="street-address" placeholder="Street Address" defaultValue={userInfo.streetAddress} readOnly/>
+                                <input type="text" className="form-control" id="street-address" placeholder="Street Address" defaultValue={userInfo.streetAddress} readOnly />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="city" className="form-label">City</label>
-                                <input type="text" className="form-control" id="city" placeholder="City" defaultValue={userInfo.city} readOnly/>
+                                <input type="text" className="form-control" id="city" placeholder="City" defaultValue={userInfo.city} readOnly />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="country" className="form-label">Country</label>
@@ -151,7 +157,7 @@ const UserPage = () => {
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="zip-code" className="form-label">Zip Code</label>
-                                <input type="text" className="form-control" id="zip-code" placeholder="Zip Code" defaultValue={userInfo.zipCode} readOnly/>
+                                <input type="text" className="form-control" id="zip-code" placeholder="Zip Code" defaultValue={userInfo.zipCode} readOnly />
                             </div>
                         </div>
 
