9163dc7c4955db6036ce20167c7e1402a3caff53
Hansel <valkirecesa@gmail.com>
2022-10-12 22:10:11 +0800

    finished API route for submitting answers - added a route for submitting answers - added a controller for submitting answers - added sklearn and scipy inside requirements.txt - added route rule inside server.py - added the dataset.csv used during the ML training - added the pickled version of the model - created an SVM_Model class inside parser.py that will perform the ff:     - parse the request object to simplify the LSAS and SPIN scores     - scale(standardize) the parsed request object because the data the model was trained was also scaled(standardized)     - transform the scaled request object to 1D array because that is what the model will accept for the predict method     - perform predictions based on the transformed and scaled request object - added a session check before running the controller for submiting answers

--- flask-server/controllers/screening/submit_answers.py
+++ flask-server/controllers/screening/submit_answers.py
@@ -1,9 +1,14 @@
-from flask import request, jsonify
+from flask import request, jsonify, session
 from machine_learning_model.parser import SVM_Model
 
 # https://stackoverflow.com/a/16664376/15440045
 # request.json: parsed JSON data
-def submit_answers():    
+def submit_answers():
+
+    user_id = session.get("user_id")
+     #Check if session exist
+    if not user_id:
+        return jsonify({"error": "Unauthorized"}), 401    
 
     request_obj = request.json
     # print(request.json)
