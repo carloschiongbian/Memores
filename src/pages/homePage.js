8c13600d438093683bdb2c48048eb4cd7351a1b6
gelzaragoza <16101416@usc.edu.ph>
2022-11-05 02:45:14 +0800

    login updates

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,4 +1,4 @@
-import "../public/css/pages/App/App.scss";
+import "../public/css/pages/App/style.scss";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -39,98 +39,42 @@ const HomePage = () => {
   };
 
   return (
-    <div className="container">
-      <div className="homepage-content">
-        <div className="homepage-left-section">
-          <div className="homepage-memores-introduction">
-            <div className="homepage-introduction">
-              <h2 className="homepage-introduction-header">What is Memores?</h2>
-              <div className="homepage-introduction-content">
-                <p className="content-one">
-                  Mindful is a web-based intelligent screening tool that
-                  healthcare professionals can use to screen patients with
-                  Social Anxiety Disorder (SAD), and store patient records. Our
-                  goal is to minimize the chances of the misdiagnosis of SAD
-                  through the use of a machine learning model. Misdiagnosing
-                  mental health disorders in general carry risks towards
-                  patients; this includes patients still not knowing what's
-                  wrong with them and patients taking incorrect medications or
-                  treatments.
-                </p>
-
-                <p className="content-two">
-                  It does not consider all experiences of social anxiety or the
-                  possible reasons why a person might be having them. This tool
-                  does not provide a formal diagnosis of Social Anxiety
-                  Disorder. Only a professional can make a diagnosis.
-                </p>
-
-                <p className="content-three">
-                  The screening test is divided into 5 sections - namely,
-                  sectlion 1: demographic, section 2: emotional, section 3:
-                  physical symptoms, section 4: Liebowitz Social Anxiety Scale
-                  (LSAS), and section 5: Social Phobia Inventory (SPIN). After
-                  taking the test, the probability of manifesting social anxiety
-                  disorder will be shown.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="homepage-right-sectio n">
-          <div className="homepage-user-login">
-            <div className="login-modal">
-              <h3 className="login-modal-header">Sign In Here</h3>
-
-              <div className="login-modal-inputs">
-                <form
-                  className="login-form"
-                  method="POST"
-                  onSubmit={handleSubmit(onSubmit)}
-                >
-                  <input
-                    {...register("user")}
-                    type="text"
-                    name="user"
-                    id="login-username"
-                    placeholder="Username"
-                  />
-                  {errors.user && (
-                    <span
-                      className="text-danger"
-                      style={{ fontSize: "12px", marginBottom: "0px" }}
-                    >
-                      {errors.user.message}
-                    </span>
-                  )}
-                  <input
-                    {...register("password")}
-                    type="password"
-                    name="password"
-                    id="login-password"
-                    placeholder="Password"
-                  />
-                  {errors.password && (
-                    <span
-                      className="text-danger"
-                      style={{ fontSize: "12px", marginBottom: "0px" }}
-                    >
-                      {errors.password.message}
-                    </span>
-                  )}
-                  <input
-                    type="submit"
-                    name="login-submit-button"
-                    id="login-submit-button"
-                  />
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div class="screen-1">
+    <input
+     {...register("user")}
+     type="text"
+     name="user"
+     id="login-username"
+     placeholder="Enter Username"
+   />
+   {errors.user && (
+     <span
+       className="text-danger"
+       style={{ fontSize: "12px", marginBottom: "0px" }}
+     >
+       {errors.user.message}
+     </span>
+   )}
+   <input
+     {...register("password")}
+     type="password"
+     name="password"
+     id="login-password"
+     placeholder="Enter Password"
+   />
+   {errors.password && (
+     <span
+       className="text-danger"
+       style={{ fontSize: "12px", marginBottom: "0px" }}
+     >
+       {errors.password.message}
+     </span>
+   )}
+  <button class="login"
+     method="POST"
+     onSubmit={handleSubmit(onSubmit)}
+   > Login </button>
+   </div> 
   );
 };
 
